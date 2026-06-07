8f989fa3771412cf297211c8db09aeb725a3b8e9
Gerhard Ahrens <gerhard.ahrens@lifeprojects.de>
2026-06-07 19:39:59 +0200

    *

---
+++ MinimalWPF_NET10_V7/TemplateCore/SourceGen/SourceGenerator.cs
@@ -0,0 +1,29 @@
+﻿namespace System.Windows
+{
+    using System;
+    using System.IO;
+    using System.Windows.Resources;
+    using System.Collections.Specialized;
+
+    internal static class SourceGenerator
+    {
+        public static void GetSourceFile()
+        {
+
+            StringCollection files = new StringCollection();
+        }
+
+        public static void GetSourceFromResources(string className)
+        {
+            Uri uri;
+            if (className.Equals("NeuUC", StringComparison.OrdinalIgnoreCase))
+            {
+                uri = new Uri("pack://application:,,,/Resources/Source/NeuUC.xaml.cs.source", UriKind.Absolute);
+
+                StreamResourceInfo sri = Application.GetResourceStream(uri);
+                using StreamReader reader = new StreamReader(sri.Stream);
+                string code = reader.ReadToEnd();
+            }
+        }
+    }
+}
