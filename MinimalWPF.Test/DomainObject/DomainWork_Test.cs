ed7d485c7f983f58d144f555fa1c1cc60ff822a3
Gerhard Ahrens <gerhard.ahrens@lifeprojects.de>
2026-07-17 09:06:59 +0200

    *

---
+++ MinimalWPF.Test/DomainObject/DomainWork_Test.cs
@@ -0,0 +1,50 @@
+﻿namespace MinimalWPF.Test
+{
+    using System.Diagnostics;
+    using System.Globalization;
+    using System.Windows;
+    using System.Windows.Domain;
+
+    using MinimalWPF.Test.Sample;
+
+    [TestClass]
+    public sealed class DomainWork_Test : BaseTest
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            CultureInfo culture = new CultureInfo("de-DE");
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
+        }
+
+        [TestMethod]
+        public void Create_Successfully()
+        {
+        }
+
+        [TestMethod]
+        public void Create_WithError()
+        {
+        }
+
+
+        [DataRow("", "")]
+        [TestMethod]
+        public void DataRowInputTest(string input, string expected)
+        {
+        }
+
+        [TestMethod]
+        public void ExceptionTest()
+        {
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+                Assert.IsTrue(ex.GetType() == typeof(Exception));
+            }
+        }
+    }
+}
