7bc9b1d73c0f5a76a126070796b33139b315c4a7
Gerhard Ahrens <Gerhard.Ahrens@lifeprojects.de>
2026-07-21 15:41:22 +0200

    Add NotifyPropertyBase

---
+++ MinimalWPF_NET10_V7/Beispiele/NotifyProperty/NotifyPropertyClass.cs
@@ -0,0 +1,14 @@
+﻿namespace MinimalWPF.Beispiele.NotifyProperty
+{
+    using System.Windows;
+
+    public class NotifyPropertyClass : NotifyPropertyBase
+    {
+        public string DemoText
+        {
+            get => base.GetValue<string>();
+            set => base.SetValue(value);
+        }
+
+    }
+}
