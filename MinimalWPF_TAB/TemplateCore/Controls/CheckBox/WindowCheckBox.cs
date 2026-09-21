65751d49241e7addd2c540b540ee5278608612db
Gerhard Ahrens <gerhard.ahrens@lifeprojects.de>
2026-09-21 19:53:10 +0200

    #20260921-20:00 Add WindowsCheckBox

---
+++ MinimalWPF_TAB/TemplateCore/Controls/CheckBox/WindowCheckBox.cs
@@ -0,0 +1,12 @@
+﻿namespace System.Windows.Controls
+{
+    using System.Windows;
+
+    public class WindowCheckBox : CheckBox
+    {
+        static WindowCheckBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(WindowCheckBox), new FrameworkPropertyMetadata(typeof(WindowCheckBox)));
+        }
+    }
+}
